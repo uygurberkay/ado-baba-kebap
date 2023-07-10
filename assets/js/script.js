caa4c00b5b8ae464e6ab455fa9d637ce0ed8f0a4
uygurberkay <50241715+uygurberkay@users.noreply.github.com>
2023-07-10 22:58:56 +0300

    v.03

--- assets/js/script.js
+++ assets/js/script.js
@@ -168,3 +168,22 @@ window.addEventListener("mousemove", function (event) {
   }
 
 });
+
+ /* SMS POST and GET methods */
+
+ const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  
+  const raw = JSON.stringify({"api_key":"API_KEY_HERE","title":"SMS_TITLE_HERE","text":"TEXT_SMS","sentto":"905010968006"});
+
+  const requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+  };
+
+  fetch("turkeysms.com.tr/api/v3/gonder/add-content", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
